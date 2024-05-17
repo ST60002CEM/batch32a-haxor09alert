aa8ad0629542a23605cc65e1c2e05415fcd03419
Alekh Chaudhary <chaudharyalekh@gmail.com>
2024-05-17 18:11:24 +0545

    splash_screen completed

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:memorymate/screens/auth_screen/login_screen/model/login_screen.dart';
+import 'package:memorymate/screens/walkthrough/splashscreen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
