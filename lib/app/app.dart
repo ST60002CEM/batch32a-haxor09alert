4002a238e953af0bed9cb3c455db2c981b9bc3a8
alekh09 <chaudharyalekh@gmail.com>
2024-06-14 12:10:52 +0545

    hive setup done

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:memorymate/features/auth_screen/login_screen/login_screen.dart';
 import 'package:memorymate/features/walkthrough/presentation/view/splash_view.dart';
 
 class App extends StatelessWidget {
---
+++ lib/app/constant/hive_table_constant.dart
@@ -0,0 +1,13 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const int usernameTableId = 0;
+  static const String usernameBox = 'usernameBox';
+
+  static const int emailTableId = 1;
+  static const String emailBox = 'emailBox';
+
+  static const int phoneTableId = 2;
+  static const String phoneBox = 'phoneBox';
+
+}
